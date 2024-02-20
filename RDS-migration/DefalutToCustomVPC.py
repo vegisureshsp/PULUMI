c5e873608007e75d4d4b33711d21787744dc1cd0
vegisureshsp <vegisureshgithub@gmail.com>
2024-02-20 20:23:54 +0530

    ..

--- RDS-migration/DefalutToCustomVPC.py
+++ RDS-migration/DefalutToCustomVPC.py
@@ -3,7 +3,7 @@ import pulumi_aws as aws
 
 # Create a new custom VPC
 custom_vpc = aws.ec2.Vpc("custom-vpc",
-    cidr_block="10.0.0.0/16",
+    cidr_block="10.100.0.0/16",
     enable_dns_support=True,
     enable_dns_hostnames=True,
     tags={"Name": "CustomVPC"})
@@ -11,12 +11,12 @@ custom_vpc = aws.ec2.Vpc("custom-vpc",
 # Create subnets in the custom VPC
 subnet1 = aws.ec2.Subnet("subnet1",
     vpc_id=custom_vpc.id,
-    cidr_block="10.0.1.0/24",
+    cidr_block="10.100.1.0/24",
     availability_zone="us-east-1a")
 
 subnet2 = aws.ec2.Subnet("subnet2",
     vpc_id=custom_vpc.id,
-    cidr_block="10.0.2.0/24",
+    cidr_block="10.100.2.0/24",
     availability_zone="us-east-1b")
 
 # Create a DB subnet group for the RDS instance
@@ -34,12 +34,12 @@ rds_instance = aws.rds.Instance("rds-instance",
     instance_class="db.t3.micro",  # Example instance class
     allocated_storage=20,           # Example allocated storage in GB
     engine="mysql",                 # Example database engine type
-    engine_version="5.7",           # Example database engine version
+    engine_version="8.3.0",           # Example database engine version
     multi_az=False,                 # Example multi-AZ deployment
     publicly_accessible=False,      # Example publicly accessible setting
     backup_retention_period=7,      # Example backup retention period in days
     monitoring_interval=60,         # Example monitoring interval in seconds
-    monitoring_role_arn="arn:aws:iam::123456789012:role/monitoring-role",  # Example monitoring role ARN
+    #monitoring_role_arn="arn:aws:iam::123456789012:role/monitoring-role",  # Example monitoring role ARN
     parameter_group_name="default.mysql5.7",  # Example parameter group name
     vpc_security_group_ids=["sg-0123456789abcdef0"],  # Example security group IDs
     option_group_name="default:mysql-5-7",           # Example option group name
